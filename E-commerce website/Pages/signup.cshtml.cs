d217827eab2d6da081b8a947e715edfc579a87a0
smease12 <scmease93@gmail.com>
2023-04-29 19:20:16 -0400

    update signup form validation messages

--- E-commerce website/Pages/signup.cshtml.cs
+++ E-commerce website/Pages/signup.cshtml.cs
@@ -7,19 +7,19 @@ namespace E_commerce_website.Pages
     public class signupModel : PageModel
     {
         [BindProperty]
-        [StringLength(60, MinimumLength = 3)]
-        [Required]
+        [StringLength(60, MinimumLength = 3, ErrorMessage ="Name must be 3 letters long and no more than 60")]
+        [Required(ErrorMessage = "Enter your name")]
         public string Name { get; set; }
         [BindProperty]
-        [Required]
+        [Required(ErrorMessage = "Enter your name")]
         public string Email { get; set; }
         [BindProperty]
-        [StringLength(60, MinimumLength = 8)]
-        [Required]
+        [StringLength(60, MinimumLength = 8, ErrorMessage ="Password must be 8 letters long and no more than 60")]
+        [Required(ErrorMessage = "Enter your password") ]
         public string Password { get; set; }
         [BindProperty]
-        [Required]
-        [StringLength(60, MinimumLength = 10)]
+        [Required(ErrorMessage = "Enter your phone number") ]
+        [StringLength(60, MinimumLength = 10, ErrorMessage = "Invalid number, please enter a valid one")]
         public string Number { get; set; }
         [BindProperty]
         [CheckBoxRequired(ErrorMessage = "You must agree to our terms and conditions")]
