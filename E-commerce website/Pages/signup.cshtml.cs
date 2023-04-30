e36fda47ca8e40167ec46de85be7e129b1261b7b
smease12 <scmease93@gmail.com>
2023-04-30 15:56:00 -0400

    create user model and use it in signup page

--- E-commerce website/Pages/signup.cshtml.cs
+++ E-commerce website/Pages/signup.cshtml.cs
@@ -1,3 +1,4 @@
+using E_commerce_website.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.ComponentModel.DataAnnotations;
@@ -7,29 +8,11 @@ namespace E_commerce_website.Pages
     public class signupModel : PageModel
     {
         [BindProperty]
-        [StringLength(60, MinimumLength = 3, ErrorMessage ="Name must be 3 letters long and no more than 60")]
-        [Required(ErrorMessage = "Enter your name")]
-        public string Name { get; set; }
-        [BindProperty]
-        [Required(ErrorMessage = "Enter your name")]
-        public string Email { get; set; }
-        [BindProperty]
-        [StringLength(60, MinimumLength = 8, ErrorMessage ="Password must be 8 letters long and no more than 60")]
-        [Required(ErrorMessage = "Enter your password") ]
-        public string Password { get; set; }
-        [BindProperty]
-        [Required(ErrorMessage = "Enter your phone number") ]
-        [StringLength(60, MinimumLength = 10, ErrorMessage = "Invalid number, please enter a valid one")]
-        public string Number { get; set; }
-        [BindProperty]
-        [CheckBoxRequired(ErrorMessage = "You must agree to our terms and conditions")]
-        public bool TermsAndCond {get; set;}
-        [BindProperty]
-        public bool Notification { get; set; }
+        public User User {get; set;}
         public void OnGet()
         {
         }
-        public void OnPost(string Name, string Email, string Password, string Number, string TermsAndCond, string Notification) 
+        public void OnPost() 
         {
             int foo = 0;
         }
