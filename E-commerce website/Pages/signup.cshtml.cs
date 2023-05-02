81b6962d0c3a12fa6715fd50d1d69c47fdeab284
smease12 <scmease93@gmail.com>
2023-05-02 16:45:14 -0400

    add create user and insert into database functionallity

--- E-commerce website/Pages/signup.cshtml.cs
+++ E-commerce website/Pages/signup.cshtml.cs
@@ -7,14 +7,36 @@ namespace E_commerce_website.Pages
 {
     public class signupModel : PageModel
     {
+        private readonly ECommerceDBContext _context;
         [BindProperty]
-        public User User {get; set;}
+        public User User { get; set; }
+
+        public signupModel(ECommerceDBContext context)
+        {
+            _context = context;
+        }
+
         public void OnGet()
         {
         }
-        public void OnPost() 
+        public async Task<IActionResult> OnPost()
         {
-            int foo = 0;
+            var emptyUser = new User();
+
+            if (await TryUpdateModelAsync<User>(
+                emptyUser,
+                "user",   // Prefix for form value.
+                 u => u.Name, u => u.Email, u => u.Password, u => u.Number, u => u.TermsAndCond, 
+                 u => u.Notification))
+            {
+                emptyUser.CreatedDate = DateTime.UtcNow;    
+                emptyUser.LastUpdatedDate = DateTime.UtcNow;    
+                _context.Users.Add(emptyUser);
+                await _context.SaveChangesAsync();
+                return RedirectToPage("/");
+            }
+
+            return Page();
         }
     }
 }
