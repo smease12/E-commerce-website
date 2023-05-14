b3eebe639c03878bbd064648140e903e5ba6a195
smease12 <scmease93@gmail.com>
2023-05-13 20:37:38 -0400

    Finish working links in user and cart popups. Popups are not spaced perfectly but are working

--- E-commerce website/wwwroot/js/home.js
+++ E-commerce website/wwwroot/js/home.js
@@ -21,3 +21,8 @@ function openPopup() {
     var popup = document.getElementById("myPopup");
     popup.classList.toggle("show");
 }
+
+function openPopup2() {
+    var popup = document.getElementById("myPopup2");
+    popup.classList.toggle("show");
+}
