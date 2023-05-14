2b0bf5da2be19284cd7a4a9df8367de5ea54db66
smease12 <scmease93@gmail.com>
2023-05-14 18:41:48 -0400

    add AddProduct page and product model

---
+++ E-commerce website/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_commerce_website.Models
+{
+    public class Product
+    {
+        [Key]
+        public int id { get; set; }
+        [Required]
+        public string name { get; set; }    
+        public string description { get; set; }
+        public string imgLocation1 { get; set; }
+        public string imgLocation2 { get; set;}
+        public string imgLocation3 { get; set;} 
+        public string imgLocation4 { get; set;}
+        public decimal fullPrice { get; set; }
+        public decimal discount { get; set; }
+        public int stock { get; set; }
+        public string tags { get; set; }
+    }
+}
