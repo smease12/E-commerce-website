1dde3dcc51377719faba7f1f20a8ce8c764d8b2c
smease12 <scmease93@gmail.com>
2023-05-19 23:00:51 -0400

    save fle locations and all other fields on add product page

--- E-commerce website/Pages/addproduct.cshtml.cs
+++ E-commerce website/Pages/addproduct.cshtml.cs
@@ -12,13 +12,13 @@ namespace E_commerce_website.Pages
         [BindProperty]
         public Product Product { get; set; }
         [BindProperty]
-        public IFormFile Img1 { get; set; }
+        public IFormFile? Img1 { get; set; }
         [BindProperty]
-        public IFormFile Img2 { get; set; }
+        public IFormFile? Img2 { get; set; }
         [BindProperty]
-        public IFormFile Img3 { get; set; }
+        public IFormFile? Img3 { get; set; }
         [BindProperty]
-        public IFormFile Img4 { get; set; }
+        public IFormFile? Img4 { get; set; }
 
         public addproductModel(ECommerceDBContext context, Microsoft.AspNetCore.Hosting.IHostingEnvironment environment) 
         {
@@ -30,33 +30,44 @@ namespace E_commerce_website.Pages
         {
         }
 
+        //upload a file and return the file location as a string
+        private string uploadFile(IFormFile img) 
+        {
+            string path = "";
+            if (img != null)
+            {
+                var file = Path.Combine(_environment.ContentRootPath, "wwwroot\\uploads", img.FileName);
+                path = file.ToString();
+                using (var fileStream = new FileStream(file, FileMode.Create))
+                {
+                    img.CopyTo(fileStream);
+                }
+            }
+            return path;
+        }
+
         public async Task<IActionResult> OnPostAsync()
         {
             var emptyProduct = new Product();
 
-            //var test = await TryUpdateModelAsync<Product>(
-            //    emptyProduct,
-            //    "product",   // Prefix for form value.
-            //    p => p.name, p => p.descriptionShort, p => p.descriptionLong,
-            //    p => p.imgLocation1, p => p.imgLocation2, p => p.imgLocation3,
-            //    p => p.imgLocation4, p => p.fullPrice, p => p.discount, p => p.stock,
-            //    p => p.tags);
-            //var modelState = ModelState.Values;
+            var test = await TryUpdateModelAsync<Product>(
+                emptyProduct,
+                "product",   // Prefix for form value.
+                p => p.name, p => p.descriptionShort, p => p.descriptionLong,
+                 p => p.fullPrice, p => p.discount, p => p.stock, p => p.tags);
+            var modelState = ModelState.Values;
 
-            var file = Path.Combine(_environment.ContentRootPath,  Img1.FileName); //"uploads",
-            using (var fileStream = new FileStream(file, FileMode.Create))
-            {
-                await Img1.CopyToAsync(fileStream);
-            }
 
             if (await TryUpdateModelAsync<Product>(
                 emptyProduct,
                 "product",   // Prefix for form value.
                 p => p.name, p=> p.descriptionShort, p => p.descriptionLong,
-                p => p.imgLocation1, p => p.imgLocation2, p => p.imgLocation3,
-                p => p.imgLocation4, p => p.fullPrice, p => p.discount, p => p.stock,
-                p => p.tags))
+                 p => p.fullPrice, p => p.discount, p => p.stock, p => p.tags))
             {
+                emptyProduct.imgLocation1 = uploadFile(Img1);
+                emptyProduct.imgLocation2 = uploadFile(Img2);
+                emptyProduct.imgLocation3 = uploadFile(Img3);
+                emptyProduct.imgLocation4 = uploadFile(Img4);
                 _dbContext.Products.Add(emptyProduct);
                 await _dbContext.SaveChangesAsync();
                 return RedirectToPage("/");
