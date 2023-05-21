1030cd89b86d553007e97ddfa522825f1110e2f0
smease12 <scmease93@gmail.com>
2023-05-20 21:59:37 -0400

    Update addproduct.js to show image preview of uploaded image before form is submitted for add product

--- E-commerce website/wwwroot/js/addproduct.js
+++ E-commerce website/wwwroot/js/addproduct.js
@@ -12,3 +12,33 @@ sellprice.addEventListener('input', () => {
     let d = (sellprice.value / actualprice.value) * 100;
     discount.value = d;
 })
+
+let uploadImages = document.querySelectorAll('.fileupload');
+let imagePaths = []; // will store all uploaded images paths;
+
+uploadImages.forEach((fileupload, index) => {
+    fileupload.addEventListener('change', () => {
+        const file = fileupload.files[0];
+        let imageUrl;
+
+        if (file.type.includes('image')) {
+            // Create a FileReader object
+            var reader = new FileReader();
+
+            // Set up the FileReader onload event
+            reader.onload = function (e) {
+                let label = document.querySelector(`label[for=${fileupload.id}]`);
+                label.style.backgroundImage = `url(${e.target.result})`;
+                let productImage = document.querySelector('.product-image');
+                productImage.style.backgroundImage = `url(${e.target.result})`;
+            };
+
+            // Read the uploaded file as a data URL
+            reader.readAsDataURL(file);
+            
+        } else {
+            showAlert('upload image only');
+        }
+    })
+})
+
