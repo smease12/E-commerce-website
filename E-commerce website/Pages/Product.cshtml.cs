dd47e2f39dad96b8b72b2fbf20eb48794ad74d9c
smease12 <scmease93@gmail.com>
2023-05-21 19:25:33 -0400

    Update product page to have productid as routing data in onget method, display data according to id given.

--- E-commerce website/Pages/Product.cshtml.cs
+++ E-commerce website/Pages/Product.cshtml.cs
@@ -1,12 +1,26 @@
+using E_commerce_website.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace E_commerce_website.Pages
 {
     public class ProductModel : PageModel
     {
-        public void OnGet()
+        private ECommerceDBContext _context;
+        public Product Product { get; set; }
+        public ProductModel(ECommerceDBContext dbContext) 
         {
+            _context = dbContext;
+        }
+        public async Task<IActionResult> OnGetAsync(int? productId)
+        {
+            Product = await _context.Products.FirstOrDefaultAsync(p => p.id == productId);
+            if (Product == null) 
+            {
+               Product = await _context.Products.FirstOrDefaultAsync(p => p.id == 9);
+            }
+            return Page();
         }
     }
 }
