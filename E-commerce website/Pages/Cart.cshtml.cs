bca6012ed69052686317ab9e1ac8b4983bc091ae
smease12 <scmease93@gmail.com>
2023-05-27 18:48:01 -0400

    put border around product info on cart page and add products list for cart page

--- E-commerce website/Pages/Cart.cshtml.cs
+++ E-commerce website/Pages/Cart.cshtml.cs
@@ -1,12 +1,25 @@
+using E_commerce_website.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace E_commerce_website.Pages
 {
     public class CartModel : PageModel
     {
-        public void OnGet()
+        [BindProperty]
+        public List<Product> Products { get; set; }
+        private readonly ECommerceDBContext _context;
+        public CartModel(ECommerceDBContext context)
         {
+            _context = context;
+        }
+        public async void OnGetAsync(int UserId)
+        {
+            Products = ( from p in _context.Products
+                      join u in _context.UserCarts on p.id equals u.Product.id
+                      select p
+                ).ToList();
         }
     }
 }
