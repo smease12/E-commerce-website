44c58da711d76db13f52757a71027d7fc5dcbddb
smease12 <scmease93@gmail.com>
2023-05-27 23:13:07 -0400

    Add form to product page, remove select scents from product page, add usercart insert onpost for product page.

--- E-commerce website/Pages/Product.cshtml.cs
+++ E-commerce website/Pages/Product.cshtml.cs
@@ -1,4 +1,5 @@
 using E_commerce_website.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -8,10 +9,13 @@ namespace E_commerce_website.Pages
     public class ProductModel : PageModel
     {
         private ECommerceDBContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+        [BindProperty]
         public Product Product { get; set; }
-        public ProductModel(ECommerceDBContext dbContext) 
+        public ProductModel(ECommerceDBContext dbContext, UserManager<ApplicationUser> userManager) 
         {
             _context = dbContext;
+            _userManager = userManager;
         }
         public async Task<IActionResult> OnGetAsync(int? productId)
         {
@@ -20,6 +24,21 @@ namespace E_commerce_website.Pages
             {
                Product = await _context.Products.FirstOrDefaultAsync(p => p.id == 9);
             }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync() 
+        {
+            ApplicationUser user = await _userManager.GetUserAsync(User);
+            
+            UserCart userCart = new UserCart();
+            userCart.DateAdded = DateTime.Now;
+            userCart.Product = 
+                await _context.Products.FirstOrDefaultAsync(p => p.id == Product.id);
+            userCart.ApplicationUser = user;
+
+            _context.Add(userCart);
+            await _context.SaveChangesAsync();
             return Page();
         }
     }
