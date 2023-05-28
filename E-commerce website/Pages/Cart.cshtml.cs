7638d3c3d330ddec0412d4e3754d37575795fabe
smease12 <scmease93@gmail.com>
2023-05-28 19:29:06 -0400

    get user products from usercarts table and show product info on cart page

--- E-commerce website/Pages/Cart.cshtml.cs
+++ E-commerce website/Pages/Cart.cshtml.cs
@@ -1,4 +1,5 @@
 using E_commerce_website.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -10,16 +11,29 @@ namespace E_commerce_website.Pages
         [BindProperty]
         public List<Product> Products { get; set; }
         private readonly ECommerceDBContext _context;
-        public CartModel(ECommerceDBContext context)
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public CartModel(ECommerceDBContext context, UserManager<ApplicationUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
-        public async void OnGetAsync(int UserId)
+        public async Task<IActionResult> OnGetAsync()
         {
-            Products = ( from p in _context.Products
-                      join u in _context.UserCarts on p.id equals u.Product.id
-                      select p
-                ).ToList();
+            ApplicationUser user = await  _userManager.GetUserAsync(User);
+
+            if (user != null)
+            {
+                var result = (from p in _context.Products
+                            join u in _context.UserCarts on p.id equals u.Product.id
+                            where u.ApplicationUser == user
+                            select p
+                    );
+
+                Products = await result.ToListAsync();
+            }
+
+            return Page();
         }
     }
 }
