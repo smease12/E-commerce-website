232cc59aa72b34fa01b3527c60be855b4579e00a
smease12 <scmease93@gmail.com>
2023-06-03 20:36:55 -0400

    add checkout page

---
+++ E-commerce website/Pages/Checkout.cshtml.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+
+//https://blog.christian-schou.dk/implement-stripe-payments-in-asp-net6/
+namespace E_commerce_website.Pages
+{
+    public class CheckoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
