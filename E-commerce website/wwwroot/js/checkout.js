280c40e389d25edf05e82fe2846b1f3ecb4a8fc5
smease12 <scmease93@gmail.com>
2023-08-01 20:46:19 -0400

    Create javascript for checkout page to update price when different quantity is chosen for a product

--- E-commerce website/wwwroot/js/checkout.js
+++ E-commerce website/wwwroot/js/checkout.js
@@ -1,9 +1,33 @@
-ProductQty.addEventListener('input', () => {
-    if (discount.value > 100) {
-        discount.value = 90;
-    } else {
-        let d = (actualprice.value * discount.value) / 100;
-        sellprice.value = actualprice.value - d;
+document.addEventListener('DOMContentLoaded', function () {
+    //Get references to all quantity dropdowns and price displays
+    const quantityDropdowns = document.querySelectorAll('[id^="quantityDropdown_"]');
+    const priceDisplays = document.querySelectorAll('[id^="priceDisplay_"]');
 
+    //Define a function to update the price based on the selected quantity
+    function updatePrice(event) {
+        const selectedQuantity = event.target.value;
+        console.log(event.target.dataset);
+        console.log(selectedQuantity);
+
+        const productId = event.target.dataset.productId;
+
+        const pricePerUnit = parseFloat(event.target.dataset.productPrice);
+        console.log(event.target);
+        console.log(pricePerUnit);
+        //Calculate the total price based on the quantity and price per unit
+        const totalPrice = selectedQuantity * pricePerUnit;
+        console.log(totalPrice);
+        //Find the corresponding price display and update it
+        const priceDisplay = document.getElementById('priceDisplay_' + productId);
+
+        priceDisplay.textContent = '$' + totalPrice.toFixed(2); //Display price with 2 decimal places
     }
-})
+
+    //Attach the "change" event listener to each quantity dropdown
+    quantityDropdowns.forEach(dropdown => {
+        dropdown.addEventListener('change', updatePrice);
+    });
+
+    //Call the updatePrice function initially to display the initial prices
+    updatePrice();
+});
