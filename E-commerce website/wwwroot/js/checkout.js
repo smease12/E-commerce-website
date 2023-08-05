d3f1a724960140786ee725a9568eb5ded877a134
smease12 <scmease93@gmail.com>
2023-08-05 19:30:01 -0400

    add more javascript to update the total price of all products combined when quantity dropdown is updated, and also to create hidden fields to update model values for price when quantity dropdown is updated

--- E-commerce website/wwwroot/js/checkout.js
+++ E-commerce website/wwwroot/js/checkout.js
@@ -2,25 +2,38 @@ document.addEventListener('DOMContentLoaded', function () {
     //Get references to all quantity dropdowns and price displays
     const quantityDropdowns = document.querySelectorAll('[id^="quantityDropdown_"]');
     const priceDisplays = document.querySelectorAll('[id^="priceDisplay_"]');
+    const hiddenPrices = document.querySelectorAll('[id^="hiddenPrice_"]');
 
     //Define a function to update the price based on the selected quantity
     function updatePrice(event) {
+        //Find previous total price and count
+        const totalSellPrice = document.getElementById('TotalSellPrice');
+        const count = document.getElementById('productCount').value;
+
         const selectedQuantity = event.target.value;
-        console.log(event.target.dataset);
-        console.log(selectedQuantity);
-
         const productId = event.target.dataset.productId;
-
         const pricePerUnit = parseFloat(event.target.dataset.productPrice);
-        console.log(event.target);
-        console.log(pricePerUnit);
         //Calculate the total price based on the quantity and price per unit
         const totalPrice = selectedQuantity * pricePerUnit;
-        console.log(totalPrice);
+
         //Find the corresponding price display and update it
         const priceDisplay = document.getElementById('priceDisplay_' + productId);
-
         priceDisplay.textContent = '$' + totalPrice.toFixed(2); //Display price with 2 decimal places
+
+        const hiddenPrice = document.getElementById('hiddenPrice_' + productId);
+        hiddenPrice.value = totalPrice.toFixed(2);
+        
+        //Add together all priceDisplays and update total price
+        let totalPriceCombined = 0.00;
+        console.log(totalPriceCombined);
+        for (i = 0; i < count; i++)
+        {
+            totalPriceCombined =  totalPriceCombined + parseFloat(hiddenPrices[i].value);
+            console.log(totalPriceCombined);
+        }
+        console.log(totalSellPrice.textContent);
+        totalSellPrice.textContent = "Items $"+totalPriceCombined.toFixed(2);
+        
     }
 
     //Attach the "change" event listener to each quantity dropdown
