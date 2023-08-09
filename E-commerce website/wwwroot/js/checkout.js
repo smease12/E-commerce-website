0b28b369723a0ee979f2d1ba96ad809b50e92729
smease12 <scmease93@gmail.com>
2023-08-08 21:59:27 -0400

    add hidden inputs and ids to all price displays, to be updated with javascript when quantity dropdown changes

--- E-commerce website/wwwroot/js/checkout.js
+++ E-commerce website/wwwroot/js/checkout.js
@@ -1,15 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
-    //Get references to all quantity dropdowns and price displays
+    //Get references to all quantity dropdowns hidden price inputs
     const quantityDropdowns = document.querySelectorAll('[id^="quantityDropdown_"]');
-    const priceDisplays = document.querySelectorAll('[id^="priceDisplay_"]');
     const hiddenPrices = document.querySelectorAll('[id^="hiddenPrice_"]');
 
     //Define a function to update the price based on the selected quantity
     function updatePrice(event) {
         //Find previous total price and count
-        const totalSellPrice = document.getElementById('TotalSellPrice');
+        const displayTotalSellPrice = document.getElementById('displayTotalSellPrice');
         const count = document.getElementById('productCount').value;
 
+        //get selected quantity, productid, and priceperunit from quantity dropdown
         const selectedQuantity = event.target.value;
         const productId = event.target.dataset.productId;
         const pricePerUnit = parseFloat(event.target.dataset.productPrice);
@@ -20,18 +20,21 @@ document.addEventListener('DOMContentLoaded', function () {
         const priceDisplay = document.getElementById('priceDisplay_' + productId);
         priceDisplay.textContent = '$' + totalPrice.toFixed(2); //Display price with 2 decimal places
 
+        //Find corresponding hidden input for price and update it
         const hiddenPrice = document.getElementById('hiddenPrice_' + productId);
         hiddenPrice.value = totalPrice.toFixed(2);
         
-        //Add together all priceDisplays and update total price
+        //Add together all priceDisplays and update total price before tax and shipping
         let totalPriceCombined = 0.00;
         console.log(totalPriceCombined);
         for (i = 0; i < count; i++)
         {
             totalPriceCombined =  totalPriceCombined + parseFloat(hiddenPrices[i].value);
         }
+        
+        displayTotalSellPrice.textContent = "Items $" + totalPriceCombined.toFixed(2);
 
-        totalSellPrice.textContent = "Items $"+totalPriceCombined.toFixed(2);
+
         
     }
 
