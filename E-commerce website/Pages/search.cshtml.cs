a9478cdc4cc477df05b48079829a19c7d3c5e1a2
smease12 <scmease93@gmail.com>
2023-08-25 20:12:08 -0400

    update search page to take a keyword as argument and select list of products based on if keyword is in the tags for that product

--- E-commerce website/Pages/search.cshtml.cs
+++ E-commerce website/Pages/search.cshtml.cs
@@ -1,3 +1,4 @@
+using E_commerce_website.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +6,17 @@ namespace E_commerce_website.Pages
 {
     public class searchModel : PageModel
     {
-        public void OnGet()
+        private ECommerceDBContext _context;
+        [BindProperty]
+        public List<Product> Products { get; set; }
+        public searchModel(ECommerceDBContext context) 
         {
+            _context = context;
+        }
+        public void OnGet(string? keyword)
+        {
+            string test = keyword;
+            Products = _context.Products.Where(p => p.tags.Contains(keyword)).ToList();
         }
     }
 }
